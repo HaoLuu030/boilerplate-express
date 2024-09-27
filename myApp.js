d463046d0a176036a49050dc71d6d3de848b8ed3
Hao Luu <luuquachgiahao99@gmail.com>
2024-09-27 10:31:55 +0700

    challenge 3

--- myApp.js
+++ myApp.js
@@ -6,9 +6,4 @@ const absolutePath = __dirname + '/views/index.html';
 app.get("/", (req, res) => {
     res.sendFile(absolutePath);
 })
-
-const PORT = 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running at http://localhost: ${PORT}`);
-})
 module.exports = app;
