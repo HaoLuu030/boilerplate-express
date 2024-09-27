c6db7958e31bf28026b8b78cde556b71ee8d5995
Hao Luu <luuquachgiahao99@gmail.com>
2024-09-27 10:30:51 +0700

    challenge 3

--- myApp.js
+++ myApp.js
@@ -1,8 +1,14 @@
 let express = require("express");
 let app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello Express");
-});
+const absolutePath = __dirname + '/views/index.html';
 
+app.get("/", (req, res) => {
+    res.sendFile(absolutePath);
+})
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost: ${PORT}`);
+})
 module.exports = app;
